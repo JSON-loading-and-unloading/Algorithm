c8039fe3ec099ffcff39aa46ec30d27d8add4fce
JINU-CHANG <jenny010925@naver.com>
2023-07-27 02:30:53 +0900

    1261 solve

--- jinwoo/week4/B_1261.java
+++ jinwoo/week4/B_1261.java
@@ -3,19 +3,35 @@ package jinwoo.week4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+
+class Point implements Comparable<Point> {
+        int x;
+        int y;
+        int cnt; // 벽을 부순 개수
+     
+        Point(int x, int y, int cnt) {
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
+        }
+     
+        @Override
+        public int compareTo(Point o) {
+            return cnt - o.cnt;
+        }
+    }
 
 public class B_1261 {
     static int N;
     static int M;
     static int[][] arr;
+    static int[][] chk;
     static int dx[] = {-1,1,0,0};
     static int dy[] = {0,0,-1,1};
     static int min = 100000;
     public static void main(String[] args) throws IOException {
-        Queue<Integer> queue = new LinkedList<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
@@ -23,6 +39,7 @@ public class B_1261 {
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
         arr = new int[N][M];
+        chk = new int[N][M];
 
         for(int i=0; i<N; i++){
             String s = br.readLine();
@@ -30,43 +47,33 @@ public class B_1261 {
                 arr[i][j] = s.charAt(j)-'0';
             }
         }
+        int cnt = bfs();
 
-        queue.offer(0);
-        queue.offer(0);
-        queue.offer(0);
-        queue.offer(0);
-        queue.offer(0);
+        System.out.println(cnt);
+    }
 
-        while(!queue.isEmpty()){
-            int x = queue.poll();
-            int y = queue.poll();
-            int x_ = queue.poll();
-            int y_ = queue.poll();
-            int d = queue.poll();
+    public static int bfs(){
+         PriorityQueue<Point> queue = new PriorityQueue<>(); // 벽을 부순 개수를 오름차순으로 정렬하도록 설정.
+         queue.offer(new Point(0, 0, 0));
+         while(!queue.isEmpty()){
+            Point p = queue.poll();
 
-            if(x==N-1 && y==M-1) {
-                if(d < min) {
-                    min = d;
-                } 
+            if (p.x == N-1 && p.y == M-1) {
+                return p.cnt;
             }
-            if(x<0 || y<0 || x>N-1 || y>M-1) continue;
-            if(!(x==0 && y==0) && x==x_ && y==y_) continue;
-            if(!(x==0 && y==0) && arr[x][y]==0) {
-                d = d-1;
+ 
+
+            if(p.x<0 || p.y<0 || p.x>N-1 || p.y>M-1) continue;
+            if(chk[p.x][p.y]==1) continue;
+            if(arr[p.x][p.y] == 1) {
+                p.cnt = p.cnt +1;
             }
-            if(arr[x][y]== -1) continue;
-            if((x==0 && y==0)) {
-                arr[x][y]= -1;
-            }
+            chk[p.x][p.y] = 1;
 
             for(int i=0;i<4;i++){
-                queue.offer(x+dx[i]);
-                queue.offer(y+dy[i]);
-                queue.offer(x);
-                queue.offer(y);
-                queue.offer(d+1);
+                queue.offer(new Point(p.x+dx[i],p.y+dy[i],p.cnt));
             }
         }
-        System.out.println(min);
+        return 0;
     }
 }
